eeb383b62d58a47b0d841906a519da96c1d64895
Meathill <meathill@gmail.com>
2020-01-16 13:31:09 +0800

    change: init project

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+.hello
+  h1 {{ msg }}
+
+  form(
+    @submit.prevent="doStart",
+  )
+    .form-group
+      label(for="number") 题目数量
+      input#number.form-control(
+        type="number",
+        v-model="number",
+        placeholder="题目数量",
+      )
+
+    .form-group
+      button.btn.btn-primary.btn-block 开始
+
+
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      number: 20,
+    };
+  },
+  methods: {
+    doStart() {
+
+    },
+  }
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+.home.mx-auto
+  img.logo.img-thumbnail.rounded-circle.mb-4(
+    alt="Mui's picture",
+    src="../assets/mui.jpg",
+  )
+  Hello-World(
+    msg="Welcome to Teacher Mui",
+  )
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
